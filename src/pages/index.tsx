f0b9de519b075a0fc92072e57f95689226d3b369
Oungseik <mhemaungthuwin@gmail.com>
2022-03-12 02:35:17 +0630

    refactor: my reconfigured first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+export default function HomePage() {
+  return <h1>Hello, World!</h1>;
+}
