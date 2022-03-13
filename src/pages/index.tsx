90a9fc4b743e0702ac14d63de251bf09c4a0fd3b
Oungseik <mhemaungthuwin@gmail.com>
2022-03-13 21:03:21 +0630

    feat: add main section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+import Head from 'next/head';
 import * as React from 'react';
-import { useRecoilValue } from 'recoil';
 
-import { sideMenuToggleState } from '@/states/states';
+import A from '@/components/utilities/A';
 
 export default function HomePage() {
-  const show = useRecoilValue(sideMenuToggleState);
-  return <h1>{show}</h1>;
+  return (
+    <>
+      <Head>
+        <title>Oung Seik Nyan - Home</title>
+      </Head>
+      <main className='bg-gray-200 font-semibold text-gray-600'>
+        <div className='mx-auto space-y-3 py-6 text-center font-playFair tracking-wider md:w-[548px]'>
+          <p>Hello, there! </p>
+          <p>
+            I&apos;m{' '}
+            <span className='relative text-xl'>
+              <span className='red-sunset absolute -inset-1 block -skew-y-3' />
+              <span className='relative font-semibold text-gray-50'>
+                Min Aung Thu Win
+              </span>
+            </span>{' '}
+            (Oung Seik Nyan)
+          </p>
+          <p>
+            Web Developer and Computer Science Enthusiast from Mawlamyine, Mon
+            State, Burma.{' '}
+            <A
+              href='/about'
+              className='border-b-2 border-sky-500/0 text-sky-500 transition duration-300 hover:border-sky-500/100'
+            >
+              More about me?
+            </A>
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }
