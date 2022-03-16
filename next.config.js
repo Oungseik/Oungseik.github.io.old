79253f81371ad58b573b8ac935128cbe538df8fd
Oungseik <mhemaungthuwin@gmail.com>
2022-03-16 22:54:23 +0630

    refactor: reuse the default config file as the initial commit

--- next.config.js
+++ next.config.js
@@ -1,55 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-// const withPWA = require("next-pwa");
-
-// --- default config --- //
-const eslint = {
-  dirs: ['src'],
-};
-
-const reactStrictMode = true;
-
-const webpack = (config) => {
-  config.module.rules.push({
-    test: /\.svg$/i,
-    issuer: /\.[jt]sx?$/,
-    use: [
-      {
-        loader: '@svgr/webpack',
-        options: {
-          typescript: true,
-          icon: true,
-        },
-      },
-    ],
-  });
-
-  return config;
-};
-// --- end of default config --- //
-
-module.exports = withPlugins(
-  [eslint],
-  [reactStrictMode],
-  [webpack],
-  [
-    optimizedImages,
-    {
-      handleImages: ['jpeg', 'png'],
-      optimizeImages: true,
-      optimizeImagesInDev: true,
-      defaultImageLoader: 'responsive-loader',
-      responsive: {
-        test: /\.(jpe?g|png)$/i,
-        adapter: require('responsive-loader/sharp'),
-        sizes: [512, 720, 1024, 1280, 1440, 1680, 2560],
-      },
-    },
-  ]
-);
-
-/*
+/** @type {import('next').NextConfig} */
 module.exports = {
   eslint: {
     dirs: ['src'],
@@ -83,4 +33,3 @@ module.exports = {
     return config;
   },
 };
-*/
