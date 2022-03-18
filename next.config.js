6a26b5993b5fc64b4f99c743953b0718f210f5b2
Oungseik <mhemaungthuwin@gmail.com>
2022-03-18 16:00:41 +0630

    chore: remove exportPathMap and github workflow

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 module.exports = {
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      "/": { page: "/" },
-    }
-  },
   eslint: {
     dirs: ['src'],
   },
