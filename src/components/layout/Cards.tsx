848cf8ab0690d53e3824027dbdf4e88a9d72696c
Oungseik <mhemaungthuwin@gmail.com>
2022-03-20 08:19:29 +0630

    refactor: add more breakpoint in cards layout to reduce over white space

--- src/components/layout/Cards.tsx
+++ src/components/layout/Cards.tsx
@@ -4,7 +4,7 @@ import Card from '../utilities/Card';
 
 function Cards() {
   return (
-    <div className='grid grid-cols-2 gap-y-8 md:grid-cols-4'>
+    <div className='grid grid-cols-2 gap-y-8 sm:grid-cols-3 md:grid-cols-4'>
       {skills.map((skill) => (
         <Card key={skill.title} {...skill} />
       ))}
