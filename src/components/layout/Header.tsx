6d56ca6481d2a084e9bbe6fe9a87e3b82e595ecb
Oungseik <mhemaungthuwin@gmail.com>
2022-03-20 10:49:32 +0630

    style: make the header stick on the top of the page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import NavItems from './Navitems';
 
 export default function Header() {
   return (
-    <header className=' sticky h-12 md:h-12'>
+    <header className='sticky top-0 z-50 h-12 md:h-12'>
       <NavItems />
     </header>
   );
