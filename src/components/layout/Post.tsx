5f1029ac6745141848bf2ac8107a71f5f3b48c74
Oungseik <mhemaungthuwin@gmail.com>
2022-03-29 13:36:27 +0630

    style: adjust whole page background color and blog post text color

--- src/components/layout/Post.tsx
+++ src/components/layout/Post.tsx
@@ -1,6 +1,6 @@
 export default function Post({ children }: { children: JSX.Element[] }) {
   return (
-    <article className='mx-auto max-w-screen-sm space-y-5 px-3 py-10 text-gray-800'>
+    <article className='mx-auto max-w-screen-sm space-y-5 px-3 py-10 text-gray-700'>
       {children}
     </article>
   );
