84ab0b3a101a4a5f7890a5e7765b88c1c1282ca9
Oungseik <mhemaungthuwin@gmail.com>
2022-03-29 13:25:42 +0630

    style: wrap the post within a component and add border-y on pre element

---
+++ src/components/layout/Post.tsx
@@ -0,0 +1,7 @@
+export default function Post({ children }: { children: JSX.Element[] }) {
+  return (
+    <article className='mx-auto max-w-screen-sm space-y-5 px-3 py-10 text-gray-800'>
+      {children}
+    </article>
+  );
+}
