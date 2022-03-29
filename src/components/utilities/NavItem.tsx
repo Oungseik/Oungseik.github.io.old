f139d1d95f748524815241de02cbb0754c425599
Oungseik <mhemaungthuwin@gmail.com>
2022-03-29 19:37:55 +0630

    fix: fix the problem of svg icon is unclickable

--- src/components/utilities/NavItem.tsx
+++ src/components/utilities/NavItem.tsx
@@ -11,15 +11,17 @@ type NavItemProps = {
 
 function NavItem({ href, current, text, icon }: NavItemProps) {
   return (
-    <li
-      className={clsxm(
-        'flex items-center space-x-1 rounded-lg px-2 py-1 text-sm font-semibold text-gray-100 transition duration-300 hover:bg-gray-600 md:text-xl',
-        current && 'text-sky-500'
-      )}
-    >
-      {icon}
+    <li>
       <Link href={href}>
-        <a>{text}</a>
+        <a
+          className={clsxm(
+            'flex items-center space-x-1 rounded-lg px-2 py-1 text-sm font-semibold text-gray-100 transition duration-300 hover:bg-gray-600 md:text-xl',
+            current && 'text-sky-500'
+          )}
+        >
+          {icon}
+          <span>{text}</span>
+        </a>
       </Link>
     </li>
   );
