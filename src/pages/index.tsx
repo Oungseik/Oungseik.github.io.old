468d688c9182209b5313e40fbbe6cb64d68c3dfc
Oungseik <mhemaungthuwin@gmail.com>
2022-07-02 17:12:12 +0630

    feat: add og tags for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,28 @@ export default function HomePage() {
   return (
     <>
       <Head>
+        <meta property='og:title' content='Oung Seik Nyan' key='ogtitle' />
+        <meta
+          property='og:description'
+          content='Personal Website'
+          key='ogdesc'
+        />
+        <meta
+          property='og:image'
+          content='../../public/images/og-img.png'
+          key='ogimage'
+        />
+        <meta
+          property='og:site_name'
+          content='Oung Seik Nyan - Home'
+          key='ogsitename'
+        />
+        <meta
+          property='og:url'
+          content='https://oungseik-github-io.vercel.app/'
+          key='ogurl'
+        />
+        <meta property='og:type' content='Personal Website' key='ogtype' />
         <title>Oung Seik Nyan - Home</title>
       </Head>
       <Hero />
